b7178c2bcdc499d6da7e4038cf07fcd2d7d7be4b
ChenYong <chenyong@jimeng.io>
2019-03-14 19:35:30 +0800

    prepare more components for exporting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+// messages
+
+export { default as AlertMessages } from "./alert-messages";
+export * from "./message/message-center";
+
 // buttons
 
 export { default as ThinButton } from "./thin-button";
@@ -7,16 +12,50 @@ export { default as LargeButton } from "./large-button";
 
 export { default as Space } from "./space";
 
-// inputs
+// inline
 
 export { default as NumberInline } from "./number-inline";
+export { default as TextareaInline } from "./textarea-inline";
 
 // forms
 
 export { default as LabelField } from "./label-field";
 export { default as LabelFieldVertical } from "./label-field-vertical";
 
-// messages
+// popup
 
-export { default as AlertMessages } from "./alert-messages";
-export * from "./message/message-center";
+export { default as InputPopup } from "./input-popup";
+export { default as InputSelectPopup } from "./input-select-popup";
+export { default as NumberPopup } from "./number-popup";
+export { default as QrcodePopup } from "./qrcode-popup";
+export { default as RangePopup } from "./range-popup";
+export { default as SelectSopup } from "./select-popup";
+export { default as TextareaPopup } from "./textarea-popup";
+export { default as TreeSelecPopup } from "./tree-select-popup";
+
+// others
+export { default as ArrowBack } from "./arrow-back";
+export { default as CardFormLayout } from "./card-form-layout";
+export { default as DataEntryRow } from "./data-entry-row";
+export { default as EmptyPlaceholder } from "./empty-placeholder";
+export { default as FloatAboveButton } from "./float-above-button";
+export { default as FooterOperations } from "./footer-operations";
+export { default as FormItemVertical } from "./form-item-vertical";
+export { default as FormSectionTitle } from "./form-section-title";
+export { default as IconLabel } from "./icon-label";
+export { default as InputInline } from "./input-inline";
+export { default as LoadingPlaceholder } from "./loading-placeholder";
+export { default as NavigatorPage } from "./navigator-page";
+export { default as NavigatorSelect } from "./navigator-select";
+export { default as ProgressPercent } from "./progress-percent";
+export { default as RoughTable } from "./rough-table";
+export { default as SamplingCounter } from "./sampling-counter";
+export { default as SwitchOkNg } from "./switch-ok-ng";
+export { default as TabButton } from "./tab-button";
+export { default as TabsBar } from "./tabs-bar";
+export { default as ThinCheckbox } from "./thin-checkbox";
+export { default as ThinDivider } from "./thin-divider";
+export { default as ThinPagination } from "./thin-pagination";
+export { default as ThinProgress } from "./thin-progress";
+export { default as ThinSubmittingTable } from "./thin-submitting-table";
+export { default as TimeFormat } from "./time-format";
