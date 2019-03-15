7c65c9a5d29ca8b1d8e529eb344f0530ebbb016a
ChenYong <chenyong@jimeng.io>
2019-03-15 15:29:06 +0800

    tweaked a simple animation for navigator-page

--- example/whiteboard/demo-navigator-page.tsx
+++ example/whiteboard/demo-navigator-page.tsx
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { SFC, useState } from "react";
+import NavigatorPage from "../../src/navigator-page";
+import { css } from "emotion";
 
-export function DemoNavigatorPage() {
-  return <div> DEBUG navigator-page </div>;
-}
+interface IProps {}
+
+let DemoNavigatorPage: SFC<IProps> = (props) => {
+  let [visible, setVisible] = useState(false);
+
+  return (
+    <div>
+      <NavigatorPage
+        visible={visible}
+        onClose={() => {
+          setVisible(false);
+        }}
+      >
+        <div
+          className={styleContainer}
+          onClick={() => {
+            setVisible(false);
+          }}
+        >
+          Close
+        </div>
+      </NavigatorPage>
+
+      <div
+        className={styleContainer}
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        Toggle
+      </div>
+    </div>
+  );
+};
+
+let styleContainer = css`
+  padding: 16px;
+`;
+
+export default DemoNavigatorPage;
--- src/navigator-page.tsx
+++ src/navigator-page.tsx
@@ -1,5 +1,82 @@
-import React from "react";
+import React, { SFC } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { css } from "emotion";
 
-export default function NavigatorPage() {
-  return <div> implemented navigator-page </div>;
+let transitionDuration = 160;
+
+interface IProps {
+  visible: boolean;
+  onClose: () => void;
 }
+
+let NavigatorPage: SFC<IProps> = (props) => {
+  return (
+    <div>
+      <TransitionGroup className={styleAnimations}>
+        {props.visible ? (
+          <CSSTransition classNames="backdrop" timeout={{ enter: transitionDuration, exit: transitionDuration }}>
+            <div className={styleBackdrop} />
+          </CSSTransition>
+        ) : null}
+        {props.visible ? (
+          <CSSTransition classNames="slider" timeout={{ enter: transitionDuration, exit: transitionDuration }}>
+            <div className={stylePopPage}>{props.children}</div>
+          </CSSTransition>
+        ) : null}
+      </TransitionGroup>
+    </div>
+  );
+};
+
+let styleAnimations = css`
+  .slider-enter {
+    left: 100%;
+    width: 100%;
+  }
+  .slider-enter.slider-enter-active {
+    left: 0%;
+  }
+  .slider-exit {
+    left: 0%;
+  }
+  .slider-exit.slider-exit-active {
+    left: 100%;
+  }
+
+  .backdrop-enter {
+    opacity: 0;
+  }
+  .backdrop-enter.backdrop-enter-active {
+    opacity: 1;
+  }
+  .backdrop-exit {
+    opacity: 1;
+  }
+  .backdrop-exit.backdrop-exit-active {
+    opacity: 0;
+  }
+`;
+
+let stylePopPage = css`
+  position: fixed;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  background-color: white;
+
+  transition: ${transitionDuration}ms;
+  transition-timing-function: linear;
+`;
+
+let styleBackdrop = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: hsla(0, 0%, 0%, 0.3);
+  transition: ${transitionDuration}ms;
+  transition-timing-function: linear;
+`;
+
+export default NavigatorPage;
